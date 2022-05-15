4db45d90ac2e7027457bfec727c1b7e143494fd3
kouheikishigami <kouhei19920522@gmail.com>
2022-05-15 21:38:51 +0900

    アカウント同士でのトークン転送を実装した

--- contracts/Mocha.sol
+++ contracts/Mocha.sol
@@ -17,32 +17,32 @@ interface EIP20 {
         external
         returns (bool success);
 
-    // /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-    // /// @param _from The address of the sender
-    // /// @param _to The address of the recipient
-    // /// @param _value The amount of token to be transferred
-    // /// @return success Whether the transfer was successful or not
-    // function transferFrom(
-    //     address _from,
-    //     address _to,
-    //     uint256 _value
-    // ) external returns (bool success);
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return success Whether the transfer was successful or not
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) external returns (bool success);
 
-    // /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
-    // /// @param _spender The address of the account able to transfer the tokens
-    // /// @param _value The amount of wei to be approved for transfer
-    // /// @return success Whether the approval was successful or not
-    // function approve(address _spender, uint256 _value)
-    //     external
-    //     returns (bool success);
+    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of wei to be approved for transfer
+    /// @return success Whether the approval was successful or not
+    function approve(address _spender, uint256 _value)
+        external
+        returns (bool success);
 
-    // /// @param _owner The address of the account owning tokens
-    // /// @param _spender The address of the account able to transfer the tokens
-    // /// @return remaining Amount of remaining tokens allowed to spent
-    // function allowance(address _owner, address _spender)
-    //     external
-    //     view
-    //     returns (uint256 remaining);
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return remaining Amount of remaining tokens allowed to spent
+    function allowance(address _owner, address _spender)
+        external
+        view
+        returns (uint256 remaining);
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(
@@ -63,6 +63,8 @@ contract Mocha is EIP20 {
 
     // 各アカウントの残高
     mapping(address => uint256) public balances;
+    // 各アカウントによる転送を許可したトークンの量
+    mapping(address => mapping(address => uint256)) private allowances;
 
     constructor(uint256 _totalSupply) {
         totalSupply = _totalSupply;
@@ -79,22 +81,65 @@ contract Mocha is EIP20 {
         return balances[_owner];
     }
 
-    function transfer(address _to, uint256 _value)
-        public
-        override
-        returns (bool success)
-    {
-        require(balances[msg.sender] >= _value, "Insufficient balance");
-        balances[msg.sender] -= _value;
+    function _transfer(
+        address _from,
+        address _to,
+        uint256 _value
+    ) private {
+        require(balances[_from] >= _value, "Insufficient balance");
+        require(_from != _to, "from = to");
+        balances[_from] -= _value;
         balances[_to] += _value;
 
         console.log(
             "transfer '%d' token from '%s' to '%s':",
             _value,
-            msg.sender,
+            _from,
             _to
         );
-        emit Transfer(msg.sender, _to, _value);
+        emit Transfer(_from, _to, _value);
+    }
+
+    function transfer(address _to, uint256 _value)
+        public
+        override
+        returns (bool success)
+    {
+        _transfer(msg.sender, _to, _value);
         return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public override returns (bool success) {
+        require(
+            allowances[_from][msg.sender] >= _value,
+            "Transfer amount exceeds allownace"
+        );
+
+        _transfer(_from, _to, _value);
+        allowances[_from][msg.sender] -= _value;
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        override
+        returns (bool success)
+    {
+        allowances[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender)
+        public
+        view
+        override
+        returns (uint256)
+    {
+        return allowances[_owner][_spender];
     }
 }
--- test/mochaToken.test.ts
+++ test/mochaToken.test.ts
@@ -7,20 +7,19 @@ import type { Mocha } from "../typechain/";
 describe("MochaToken", function () {
   let mochaToken: Mocha;
   let owner: SignerWithAddress;
-  let addr1: SignerWithAddress;
-  let addr2: SignerWithAddress;
+  let user1: SignerWithAddress;
+  let user2: SignerWithAddress;
 
   const TOTAL_SUPPLY = ethers.BigNumber.from(10 ** 4);
 
   const setUp = async () => {
-    console.log("---- before ----");
     const MochaFactory = await ethers.getContractFactory("Mocha");
     mochaToken = await MochaFactory.deploy(TOTAL_SUPPLY);
 
     const accounts = await ethers.getSigners();
     owner = accounts[0];
-    addr1 = accounts[1];
-    addr2 = accounts[2];
+    user1 = accounts[1];
+    user2 = accounts[2];
   };
 
   describe("Basic token test", function () {
@@ -50,10 +49,10 @@ describe("MochaToken", function () {
 
     it("Should have correct initial balances", async () => {
       const ownerBalance = await mochaToken.balanceOf(owner.address);
-      const addr1Balance = await mochaToken.balanceOf(addr1.address);
+      const user1Balance = await mochaToken.balanceOf(user1.address);
 
       assert(ownerBalance.eq(TOTAL_SUPPLY));
-      assert(addr1Balance.eq(0));
+      assert(user1Balance.eq(0));
     });
   });
 
@@ -70,23 +69,49 @@ describe("MochaToken", function () {
       await expect(
         mochaToken
           .connect(owner)
-          .transfer(addr1.address, ownerBalance.add(transferAmount))
+          .transfer(user1.address, ownerBalance.add(transferAmount))
       ).to.be.revertedWith("Insufficient balance");
     });
 
     it("Should pass when transfer amount <= balance", async () => {
       assert(
-        await mochaToken.connect(owner).transfer(addr1.address, transferAmount)
+        await mochaToken.connect(owner).transfer(user1.address, transferAmount)
       );
     });
 
     it("Should update balances", async () => {
       const ownerBalance = await mochaToken.balanceOf(owner.address);
-      const addr1Balance = await mochaToken.balanceOf(addr1.address);
+      const user1Balance = await mochaToken.balanceOf(user1.address);
       const totalSupply = await mochaToken.connect(owner).totalSupply();
 
       assert(totalSupply.sub(transferAmount).eq(ownerBalance))
-      assert(addr1Balance.eq(transferAmount))
+      assert(user1Balance.eq(transferAmount))
     });
   });
+
+  describe("transferFrom test", function () {
+
+
+    before(async function () {
+      await setUp();
+      await mochaToken
+        .connect(owner)
+        .transfer(user2.address, 500)
+      await mochaToken.connect(user2).approve(user1.address, ethers.BigNumber.from(500))
+    });
+
+    it("Should revert when transfer amount > allowance", async () => {
+      await expect(
+        mochaToken.connect(user1).transferFrom(user2.address, user1.address, ethers.BigNumber.from(501))
+      ).to.be.revertedWith("Transfer amount exceeds allownace");
+    })
+
+    it("Should pass when transfer amount =< allowance", async () => {
+      const approveAmount = await mochaToken.allowance(user2.address, user1.address);
+
+      assert(
+        await mochaToken.connect(user1).transferFrom(user2.address, user1.address, approveAmount)
+      )
+    })
+  })
 });
